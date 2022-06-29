57434f23fa1b689b0bb2fe66b20d053720b5b3ac
raghavendra1991 <92263068+raghavendra1991@users.noreply.github.com>
2022-06-29 21:18:01 +0530

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -4,3 +4,4 @@ export http_proxy=http://127.0.0.1:3128/
 export https_proxy=http://127.0.0.1:3128/
 export ftp_proxy=http://127.0.0.1:3128/
 export socks_proxy=socks://127.0.0.1:3128/
+curl google.de
