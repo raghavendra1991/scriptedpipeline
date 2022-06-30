d5099b36782b8cec32fec499bf471b774e5214a0
raghavendra1991 <92263068+raghavendra1991@users.noreply.github.com>
2022-06-30 09:24:15 +0530

    Delete proxy.sh

--- proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# CNTLM Proxy settings
-export http_proxy=http://127.0.0.1:3128/
-export https_proxy=http://127.0.0.1:3128/
-export ftp_proxy=http://127.0.0.1:3128/
-export socks_proxy=socks://127.0.0.1:3128/
-curl google.de
